919965315efe4d7bc8c64e5c048f8f7aedd5ec62
ueno <ueno@nayuta.co>
2019-03-16 03:30:59 +0000

    JSON double quote

---
+++ htlcchanged.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -eu
+#   method: htlc_changed
+#   $1: short_channel_id
+#   $2: node_id
+#   $3: local_msat
+DATE=`date -u +"%Y-%m-%dT%H:%M:%S.%N"`
+JSON=`cat << EOS | jq -e '.'
+{ "method":"htlc_changed", "short_channel_id":"$1", "node_id":"$2", "date":"$DATE", "local_msat":$3 }
+EOS
+`
+
+echo ${JSON}
+python /home/nayuta/lns_test_mqtt/mqtt_pub.py result "${JSON}"
+
