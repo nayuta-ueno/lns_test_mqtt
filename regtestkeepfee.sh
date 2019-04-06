bbd4e104f608cc7f057b2b7b5808d19f881e1296
Ubuntu <nayuta@lntest2.vakcta53mxeuhm1rmq2nlnidna.lx.internal.cloudapp.net>
2019-04-06 05:43:53 +0000

    regtest tool

---
+++ regtestkeepfee.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+GENERATE_SEC=20
+
+CHAIN=`bitcoin-cli getblockchaininfo | jq -r .chain`
+echo ${CHAIN}
+if [ ${CHAIN} != regtest ]; then
+	echo REGTEST only
+	exit 1
+fi
+
+cnt=0
+while :
+do
+	FEERATE=`bitcoin-cli estimatesmartfee 6 | jq .feerate`
+	#echo FEERATE=${FEERATE}
+	if [ ${FEERATE} != null ]; then
+		FEERATE=$(printf "%.8f" ${FEERATE})
+		SUB=`echo "${FEERATE} - 0.00005000" | bc`
+		#echo $SUB
+		if [ ${SUB:0:1} == '-' ]; then
+			echo FEERATE=${FEERATE}
+			sleep ${GENERATE_SEC}
+			bitcoin-cli generate 1 > /dev/null
+			cnt=0
+			continue
+		fi
+	fi
+	cnt=$((cnt+1))
+	if [ ${cnt} -gt 10 ]; then
+		bitcoin-cli generate 1
+		cnt=0
+	fi
+
+	LIST=`bitcoin-cli listunspent`
+	TXID=`echo ${LIST} | jq .[0].txid`
+	VOUT=`echo ${LIST} | jq .[0].vout`
+	AMOUNT=`echo ${LIST} | jq .[0].amount`
+	AMOUNT=`echo ${AMOUNT} - 0.000002 | bc`
+	AMOUNT=$(printf "%.8f" ${AMOUNT})
+	ADDR=`bitcoin-cli getnewaddress`
+
+	TX=`bitcoin-cli createrawtransaction "[{\"txid\":${TXID},\"vout\":${VOUT}}]" "[{\"${ADDR}\":${AMOUNT}}]"`
+	TX=`bitcoin-cli signrawtransactionwithwallet ${TX}`
+	TX=`echo ${TX} | jq -r .hex`
+	TXID=`bitcoin-cli sendrawtransaction ${TX}`
+	#echo ${TXID}
+done
